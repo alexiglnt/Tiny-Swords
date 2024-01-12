42ecc73b16cae68da9e8da62453234e3317aa544
alexiglnt <alexigallonet@gmail.com>
2024-01-12 12:13:17 +0100

    Update UI/sons

--- Assets/Alexi/Scripts/PauseMenu.cs
+++ Assets/Alexi/Scripts/PauseMenu.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-
+    public AudioMixer audioMixer;
     public GameObject pauseMenuUI;
+    private float previousVolume;
+    bool isMuted = false;
+
+    public GameObject retractableBanner;
+    bool isBannerOpened = false;
+
+
 
     // Update is called once per frame
     void Update()
@@ -37,5 +46,58 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         GameIsPaused = true;
     }
+
+    void TurnOffVolume()
+    {
+        // Enregistrez le volume actuel
+        audioMixer.GetFloat("volume", out previousVolume);
+
+        // Baissez le volume
+        audioMixer.SetFloat("volume", -40);
+        isMuted = true;
+    }
+
+    void RestoreVolume()
+    {
+        // Rétablissez le volume précédent
+        audioMixer.SetFloat("volume", previousVolume);
+        isMuted = false;
+    }
+
+    public void SwitchVolume()
+    {
+        if (isMuted)
+        {
+            RestoreVolume();
+        }
+        else
+        {
+            TurnOffVolume();
+        }
+    }
+
+    public void SwitchBanner()
+    {
+        Debug.Log("SwitchBanner()");
+
+        if (isBannerOpened)
+        {
+            Vector3 currentPosition = retractableBanner.transform.position;
+            float translationAmount = 130f; // Ajustez la valeur selon vos besoins
+            retractableBanner.transform.position = new Vector3(currentPosition.x + translationAmount, currentPosition.y, currentPosition.z);
+            
+            isBannerOpened = false;
+        }
+        else
+        {
+            isBannerOpened = true;
+
+            // Faites la translation vers la gauche de quelques pixels
+            Vector3 currentPosition = retractableBanner.transform.position;
+            float translationAmount = -130f; // Ajustez la valeur selon vos besoins
+            retractableBanner.transform.position = new Vector3(currentPosition.x + translationAmount, currentPosition.y, currentPosition.z);
+        }
+    }
+
 
 }
