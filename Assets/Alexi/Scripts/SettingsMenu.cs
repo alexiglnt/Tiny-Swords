51e334a3cef0c5a45eb2d8884a4412bfc0843453
alexiglnt <alexigallonet@gmail.com>
2024-02-12 17:17:04 +0100

    Color Blind filters and modes added

--- Assets/Alexi/Scripts/SettingsMenu.cs
+++ Assets/Alexi/Scripts/SettingsMenu.cs
@@ -12,12 +12,14 @@ public class GameSettings
     public int resolutionIndex;
     public int qualityIndex;
     public bool isFullScreen;
+    public int colorBlindIndex;
 }
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public TMPro.TMP_Dropdown resolutionDropdown;
+    public TMPro.TMP_Dropdown colorBlindDropdown;
     public Slider volumeSlider;
     public Toggle fullscreenToggle;
 
@@ -28,7 +30,8 @@ public class SettingsMenu : MonoBehaviour
     {
         LoadSettingsFromJson();  // Charger les paramètres avant le Start des autres scripts
 
-        Debug.Log("AWAKE - Volume: " + gameSettings.volume + ", Resolution Index: " + gameSettings.resolutionIndex + ", Quality Index: " + gameSettings.qualityIndex + ", Fullscreen: " + gameSettings.isFullScreen);
+        //Debug.Log("AWAKE - Volume: " + gameSettings.volume + ", Resolution Index: " + gameSettings.resolutionIndex + ", Quality Index: " + gameSettings.qualityIndex + ", Fullscreen: " + gameSettings.isFullScreen);
+        Debug.Log("AWAKE - COLORBLIND : " + gameSettings.colorBlindIndex);
     }
 
     private void Start()
@@ -53,6 +56,25 @@ public class SettingsMenu : MonoBehaviour
         resolutionDropdown.AddOptions(options);
 
         Debug.Log("START - Volume: " + gameSettings.volume + ", Resolution Index: " + gameSettings.resolutionIndex + ", Quality Index: " + gameSettings.qualityIndex + ", Fullscreen: " + gameSettings.isFullScreen);
+
+
+        // Créer une liste de noms de types de daltonisme dans le même ordre que l'énumération d'origine
+        List<string> colorBlindModeNames = new List<string>
+        {
+            "Normal",
+            "Protanopia",
+            "Protanomaly",
+            "Deuteranopia",
+            "Deuteranomaly",
+            "Tritanopia",
+            "Tritanomaly",
+            "Achromatopsia",
+            "Achromatomaly"
+        };
+
+        // Ajouter ces noms en tant qu'options au dropdown
+        colorBlindDropdown.AddOptions(colorBlindModeNames);
+
         UpdateUI();
     }
 
@@ -66,6 +88,7 @@ public class SettingsMenu : MonoBehaviour
         gameSettings.resolutionIndex = resolutionDropdown.value;
         gameSettings.qualityIndex = QualitySettings.GetQualityLevel();
         gameSettings.isFullScreen = Screen.fullScreen;
+        gameSettings.colorBlindIndex = colorBlindDropdown.value;
 
         // Sauvegarder les paramètres dans le fichier JSON
         SaveIntoJson();
@@ -75,10 +98,14 @@ public class SettingsMenu : MonoBehaviour
     {
         // Variable temporaire car ca ne fonctionne pas directement avec gameSettings.resolutionIndex (jsp pk)
         int resolutionIndexValue = gameSettings.resolutionIndex;
+        int colorBlindIndexValue = gameSettings.colorBlindIndex;
+
+        Debug.Log("ICIIII" + colorBlindIndexValue);
 
         volumeSlider.value = gameSettings.volume;
         resolutionDropdown.value = resolutionIndexValue;
         fullscreenToggle.isOn = gameSettings.isFullScreen;
+        colorBlindDropdown.value = colorBlindIndexValue;
     }
 
     public void SaveIntoJson()
@@ -105,7 +132,8 @@ public class SettingsMenu : MonoBehaviour
         if (!string.IsNullOrEmpty(json))
         {
             gameSettings = JsonUtility.FromJson<GameSettings>(json);
-            Debug.Log("JSON loaded - Volume: " + gameSettings.volume + ", Resolution Index: " + gameSettings.resolutionIndex + ", Quality Index: " + gameSettings.qualityIndex + ", Fullscreen: " + gameSettings.isFullScreen);
+            // Debug.Log("JSON loaded - Volume: " + gameSettings.volume + ", Resolution Index: " + gameSettings.resolutionIndex + ", Quality Index: " + gameSettings.qualityIndex + ", Fullscreen: " + gameSettings.isFullScreen);
+            Debug.Log("COLORBLIND : " + gameSettings.colorBlindIndex);
 
         }
         else
@@ -118,7 +146,8 @@ public class SettingsMenu : MonoBehaviour
                 volume = 1f,  // Volume maximal par défaut
                 resolutionIndex = 0,  // Première résolution par défaut
                 qualityIndex = 2,  // Qualité moyenne par défaut
-                isFullScreen = true  // Mode plein écran par défaut
+                isFullScreen = true,  // Mode plein écran par défaut
+                colorBlindIndex = 0  // Mode normal par défaut
             };
         }
     }
@@ -130,6 +159,16 @@ public class SettingsMenu : MonoBehaviour
         // SaveIntoJson();  // Sauvegarder les paramètres après modification
         ApplySettings();
     }
+
+    public void SetColorBlind(int colorBlindIndex)
+    {
+        // Mettre à jour la valeur de colorBlindIndex dans gameSettings avec la nouvelle valeur
+        gameSettings.colorBlindIndex = colorBlindIndex;
+
+        // Sauvegarder les paramètres dans le fichier JSON
+        SaveIntoJson();
+    }
+
 
     public void SetResolutionDropdownValue(int dropdownIndex)
     {
